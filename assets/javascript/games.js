aac0b74ab34f82f4c90479405921aa1b3bfcd8bc
jbhafner <brian_hafner@yahoo.com>
2017-08-17 10:43:21 -0700

    corrects html spacing

--- assets/javascript/games.js
+++ assets/javascript/games.js
@@ -1,4 +1,4 @@
-// Create psychic Object
+// *** Create psychic Object Section ***
 
 	var psychic = {
 		win: 0,
@@ -22,10 +22,10 @@
 
 	};
 
-	console.log ("Result of defining object below");
+	console.log ("Result of defining object below:");
 	rewriteStats()
 
-// Define Functions 
+// *** Define Functions Section ***
 
 	var randomString = function(length) {
 	    var text = "";
@@ -38,64 +38,59 @@
 
 	function rewriteStats() {
 		console.log(" /// Win = " + psychic.win + ", Losses = " + psychic.losses + ", Guess Remaining = " + psychic.guessLeft + ", Total Guess " + psychic.totalGuess + ", Selected Characters = " + psychic.selectedCharacters + ", User input is: "  + psychic.lastKeyPressed + ", Computer Input Is: " + rs + " ///");
-		// console.log("Losses = " + psychic.losses);
-		// console.log("Guess Remaining = " + psychic.guessLeft);
-		// console.log("Total Guesses = " + psychic.totalGuess);
-		// console.log("Selected Characters = " + psychic.selectedCharacters);
-		// console.log("User input is: "  +psychic.lastKeyPressed);
-		// console.log("Computer Input Is: " + rs);
-		// console.log("///// End of function rewriteStats /////");
 	}
-// Program Logic
 
-		// Generate computer number
+// *** Program Logic Section ***
 
-		var rs = randomString(1);
+// Generate computer number
 
-		// listen for onkeyup event
+var rs = randomString(1);
 
-		document.onkeyup=function(e) {
+// listen for onkeyup event
 
+document.onkeyup=function(e) {
 
-			psychic.lastKeyPressed = e.key;
-			psychic.selectedCharacters.push(e.key);
+	// recrord keypress by user
+	psychic.lastKeyPressed = e.key;
+	psychic.selectedCharacters.push(e.key);
 
-			psychic.guessLeft--;
-			console.log("--- Guess left: " + psychic.guessLeft + " ---");
-			psychic.totalGuess++;
+	// decrement the number of guesses left
+	psychic.guessLeft--;
+	console.log("--- Guess left: " + psychic.guessLeft + " ---");
+	psychic.totalGuess++;
 
-			// If user guesses the letter and wins
-			if (psychic.lastKeyPressed === rs) {
-				console.log("You win!");
-				psychic.win++;
-				psychic.guessLeft = 5;
-				console.log("--- Guess left: " + psychic.guessLeft + " ---");
-				psychic.totalGuess = 0;
-				psychic.selectedCharacters = [];
-				rs=randomString(1);
-			}
+	// If user guesses the letter and wins
+	if (psychic.lastKeyPressed === rs) {
+		console.log("You win!");
+		psychic.win++;
+		psychic.guessLeft = 5;
+		console.log("--- Guess left: " + psychic.guessLeft + " ---");
+		psychic.totalGuess = 0;
+		psychic.selectedCharacters = [];
+		rs=randomString(1);
+	}
 
-			// If user reaches maximum guesses and loses
-			if (psychic.guessLeft == 0) {
-				console.log("You Lost :-(");
-				psychic.losses++;
-				psychic.guessLeft=5;
-				psychic.totalGuess=0;
-				psychic.selectedCharacters=[];
-				rs=randomString(1);
-			}
+	// If user reaches maximum guesses and loses
+	if (psychic.guessLeft == 0) {
+		console.log("You Lost :-(");
+		psychic.losses++;
+		psychic.guessLeft=5;
+		psychic.totalGuess=0;
+		psychic.selectedCharacters=[];
+		rs=randomString(1);
+	}
 
-			// Write results to index.html
-			var html = "<p><h1>The Psychic Game</h1></p>" + 
-			"<p><h4>Guess what letter I\'m thinking of</h4></p>" + 
-			"<p><h4>Wins: " + psychic.win + 
-			"</h4></p>" + "<p><h4>Losses: " + psychic.losses + 
-			"</h4></p>" + "<p><h4>Guesses Left: " + psychic.guessLeft + 
-			"</h4></p>" + "<p><h4>Your guesses so far: " + psychic.selectedCharacters + "</h4></p>";
-			// place html into the game ID
-			document.querySelector("#game").innerHTML = html;
-			rewriteStats();
-		};
+	// Write results to index.html
+	var html = "<p><h1>The Psychic Game</h1></p>" + 
+	"<p><h4>Guess what letter I\'m thinking of</h4></p>" + 
+	"<p><h4>Wins: " + psychic.win + 
+	"</h4></p>" + "<p><h4>Losses: " + psychic.losses + 
+	"</h4></p>" + "<p><h4>Guesses Left: " + psychic.guessLeft + 
+	"</h4></p>" + "<p><h4>Your guesses so far: " + psychic.selectedCharacters + "</h4></p>";
+	// place html into the game ID
+	document.querySelector("#game").innerHTML = html;
+	rewriteStats();
+};
 
 
 
